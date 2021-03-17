511f1a66583d08d34edac32d767a1094fc61df2a
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-03-16 22:01:34 -0300

    Componente: Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./conponents/Header";
 import { GlobalStyle } from "./styles/global";
+import { Dashboard } from './conponents/Dashboard'
 
 export function App() {
   return (
     <>      
       <Header/>
+        <Dashboard />
       <GlobalStyle />
     </>
   );
