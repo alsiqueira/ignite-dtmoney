872556bb678e921d0dfdf274d080e09adf332f7d
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-03-17 19:29:41 -0300

    Configurando MirageJS

--- src/conponents/TransactionsTable/index.tsx
+++ src/conponents/TransactionsTable/index.tsx
@@ -1,33 +1,38 @@
-import { Container } from "./styles";
+import { useEffect } from 'react';
+import { Container } from './styles';
 
 export function TransactionsTable() {
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <th>Titulo</th>
-                    <th>Valor</th>
-                    <th>Categoria</th>
-                    <th>Data</th>
-                </thead>
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
-                <tbody>
-                  <tr>
-                      <td>Desenvolvimento web site</td>
-                      <td className="deposit">R$12.000</td>
-                      <td>Desenvolvimento</td>
-                      <td>16/03/2021</td>
-                  </tr>
-                  <tr>
-                      <td>Aluguel</td>
-                      <td className="withdraw">- R$1.100</td>
-                      <td>Casa</td>
-                      <td>10/03/2021</td>
-                  </tr>
-                  
-                  
-                </tbody>
-            </table>
-        </Container>
-    )
+  return (
+    <Container>
+      <table>
+        <thead>
+          <th>Titulo</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento web site</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>16/03/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.100</td>
+            <td>Casa</td>
+            <td>10/03/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }
