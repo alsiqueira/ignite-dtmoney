183a674d44c578d8cc8ca948d17b5a8f4c22b31f
Alexandre Siqueira <alexandre1760@hotmail.com>
2021-03-18 19:49:23 -0300

    Salvando dados do form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
@@ -16,7 +16,16 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTrasactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
   const [type, setType] = useState('deposit');
+  const [category, setCategory] = useState('');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({ title, value, type, category });
+  }
   return (
     <Modal
       isOpen={isOpen}
@@ -32,10 +41,19 @@ export function NewTransactionModal({
           className="react-modal-close"
         />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transações</h2>
-        <input placeholder="Título" />
-        <input type="number" placeholder="Valor" />
+        <input
+          placeholder="Título"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
         <TransactionTypeContainer>
           <RadioBox
             type="button"
@@ -61,7 +79,11 @@ export function NewTransactionModal({
             <span>Saida</span>
           </RadioBox>
         </TransactionTypeContainer>
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
         <button type="submit">Cadatrar</button>
       </Container>
     </Modal>
